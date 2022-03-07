e927bd92e3f1f950f194987a60fdaeaa0ad54e7c
Matt Carter <mcarter@businesscloudintegration.co.uk>
2022-03-07 10:44:39 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Messaging.ServiceBus;
+
+
+Console.WriteLine($"Starting");
+
+
+var serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
+var queuename = Environment.GetEnvironmentVariable("QueueName");
+
+var _serviceBusClient = new ServiceBusClient(serviceBusConnectionString);
+var _messageReceiver = _serviceBusClient.CreateReceiver(queuename);
+
+
+Console.WriteLine($"Waiting for message on queue {queuename}");
+
+var message = await _messageReceiver.ReceiveMessageAsync();
+
+
+if (message != null)
+{
+    Console.WriteLine("Got message.");
+
+    Console.WriteLine($"Contents: {message.Body}");
+    await _messageReceiver.CompleteMessageAsync(message);
+    Console.WriteLine("Message completed.");
+}
+else
+{
+    Console.WriteLine("No message received.");
+}
+
+
+Console.WriteLine("Done.");
